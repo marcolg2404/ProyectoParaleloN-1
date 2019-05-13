0004b951eefd179a84b73bd49fa10bef719af65a
marcolg2404 <marco.garridom@utem.cl>
2019-05-13 12:09:27 -0400

    Subida de prueba

---
+++ prueba.cpp
@@ -0,0 +1,11 @@
+#include <mpi.h>
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <cmath>
+
+using namespace std;
+int main(){
+  cout<<"Hola mundo"<<endl;
+}
