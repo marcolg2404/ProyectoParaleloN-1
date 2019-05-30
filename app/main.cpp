96a8cda0f8e0d12b882dc68f84102b0163e74838
marcolg2404 <marco.garridom@utem.cl>
2019-05-29 22:53:05 -0400

    Se agrega la libreria xlsxwriter para la escritura de archivos xlsx

--- app/main.cpp
+++ app/main.cpp
@@ -1,44 +1,91 @@
 #include <iostream>
-#include <xlnt/xlnt.hpp>
+#include <string>
+#include <vector>
+
+/*Cabezera funciones*/
+#include "funciones.h"
+/*Cabezeras para manejos de archivos .xlsx*/
+#include <xlnt/xlnt.hpp>//Para leer archivos
+#include "xlsxwriter.h"//Para escribir archivo final
 
 using namespace std;
+using namespace xlnt;
 
-const char *Docentes = "./Archivos/Docentes.xlsx";
-const char *Salas = "./Archivos/Salas.xlsx";
-const char *Cursos = "./Archivos/Cursos.xlsx";
-
-int main(int argc, const char **argv)/*Se pasan los argumentos. El primer argumento de la función argc nos indica el número total de parámetros.
-Mediante el segundo argumento argv, podemos acceder a los valores de los parámetros pasados al programa, siempre el primer parámetro,
- es decir argv[0] contiene el nombre del programa.*/
+int main(int argc, char **argv)/*Se pasan los argumentos. El primer argumento de la función argc nos indica el número total de parámetros.
+                                        Mediante el segundo argumento argv, podemos acceder a los valores de los parámetros pasados al programa, siempre el primer parámetro,
+                                        es decir argv[0] contiene el nombre del programa.*/
 {
-    if (argc >= 3){ /*En esta función especifica a que si no se pasan los tres archivos Salas.xlsx Docentes.xlsx Cursos.xlsx no los leerá*/
-      argv[1]=Docentes; //El vector 1 corresponde a Docentes.
-      argv[2]=Salas; //El vector 2 corresponde a Salas.
-      argv[3]=Cursos; //El vector 3 corresponde a Cursos.
-    }
-      else{
-      printf("No se ingresaron los 3 archivos .xlsx correspondientes (Salas.xlsx, Docentes.xlsx y Cursos.xlsx\n");
-      return 1;
-      }
-    xlnt::workbook wb_Docentes;
-    xlnt::workbook wb_Salas;
-    xlnt::workbook wb_Cursos;
+        /*En esta función especifica a que si no se pasan los tres archivos Salas.xlsx Docentes.xlsx Cursos.xlsx no los leerá*/
 
-    /*Se procede a cargar los archivos correspondientes*/
-    wb_Docentes.load(Docentes);
-    wb_Salas.load(Salas);
-    wb_Cursos.load(Cursos);
+        if (argc >= 7) {
 
-    auto ws = wb_Docentes.active_sheet();
+                workbook wb_Docentes; //woorkbook=archivo, worksheet=hoja
+                workbook wb_Salas;
+                workbook wb_Cursos;
 
-    cout << "Processing spread sheet" << endl;
-    for (auto row : ws.rows(false))
-    {
-        for (auto cell : row)
-    {
-        clog << cell.to_string() << endl;
-    }
-    }
-    cout << "Processing complete" << endl;
-    return 0;
+
+
+                for (int j = 1; j < argc; j=j+2) {
+                        char *argumento = argv[j];
+                        if(argumento[1]=='c') {
+                                wb_Cursos.load(argv[j+1]);
+                        }
+                        if(argumento[1]=='d') {
+                                wb_Docentes.load(argv[j+1]);
+                        }
+                        if(argumento[1]=='s') {
+                                wb_Salas.load(argv[j+1]);
+                        }
+                }
+                auto ws = wb_Salas.active_sheet();
+
+                /* Como conocer las hojas dentro del archivo
+                vector<string>prueba=wb_Salas.sheet_titles();
+                for(int i;i<prueba.size();i++){
+                cout<<prueba[i]<<endl;
+
+                } */
+
+                cout << "Procesando Salas" << endl;
+                int aux=0;
+                string sala;
+
+                vector<string>vectorCursos_filas;
+                for (auto fila : ws.rows(false)) //row=fila
+                {
+                        for (auto celda : fila) //cell=celda
+                        {
+                          aux++;
+                          if(aux>2){ //Se omiten las cabeceras del excel
+                          vectorCursos_filas.push_back(celda.to_string());
+                        }
+                      }
+                }
+
+                lxw_workbook *workbook=workbook_new("Horarios.xlsx");
+
+                for(int i=0;i<vectorCursos_filas.size();i=i+2)
+                {
+                    sala=(vectorCursos_filas[i]+"-"+vectorCursos_filas[i+1]);
+                    /*se pasa el string sala a char*/
+                    int n= sala.length()+1;
+                    char Sala_char[n];
+                    for(int k=0; k<sala.length(); k++){
+                      Sala_char[k] = sala[k];
+                    }
+
+                    lxw_worksheet *worksheet = workbook_add_worksheet(workbook,Sala_char); //Se crea una nueva hoja con el titulo de la sala
+                    cout<<sala<<endl;
+                }
+                workbook_close(workbook); //Se guarda el archivo
+                cout << "Proceso completado" << endl;
+                return 0;
+
+              }
+
+        else{
+                printf("No se ingresaron los 3 archivos .xlsx correspondientes (-s Salas.xlsx, -c Cursos.xlsx y -d Docentes.xlsx)\n");
+                return 1;
+        }
+
 }
