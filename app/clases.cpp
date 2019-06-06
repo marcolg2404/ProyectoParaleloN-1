866f6a5d0a5012c6e0d773fc1a253444e5e5dcaf
Sebaolivares11 <Sebastian.olivaresp11@gmail.com>
2019-06-06 16:15:34 -0400

    Prueba numero 3 de clases profesores, error el dia sabado,menos lineas

--- app/clases.cpp
+++ app/clases.cpp
@@ -33,11 +33,11 @@ Profesor::Profesor(string _id, string _nombre,vector<string> _bloques){
   id_profe = _id;
   nombre = _nombre;
   lunes = _bloques;
-  martes = _bloques;
+  /*martes = _bloques;
   miercoles = _bloques;
   jueves = _bloques;
   viernes = _bloques;
-  sabado = _bloques;
+  sabado = _bloques;*/
 
 }
 void Profesor::mostrar(){ // funcion para mostrar el contenido de cada objeto
@@ -47,7 +47,23 @@ void Profesor::mostrar(){ // funcion para mostrar el contenido de cada objeto
     cout << lunes[i] << "\t";
   }
 cout << endl;
+for (int i=0 ;i<martes.size();i++)
+{
+  cout << martes[i] << "\t";
 }
+cout << endl;
+for (int i=0 ;i<miercoles.size();i++)
+{
+  cout << miercoles[i] << "\t";
+}
+cout << endl;
+for (int i=0 ;i<sabado.size();i++)
+{
+  cout << sabado[i] << "\t";
+}
+cout << endl;
+}
+
 void Profesor::set_martes(vector<string> _ma){
     martes=_ma;
 }
@@ -93,7 +109,7 @@ int main(int argc, char const *argv[]) {
   string id_p,nombre_p;
   int contador=0;
   workbook wb;
-  vector<string> dias = {"Lunes","Martes","Miércoles","Jueves","Viernes","Sábado"};
+  vector<string> dias = {"Lunes","Martes","Miércoles","Jueves","Viernes","Sábado"};// aca la idea esque cuando abra el archivo lleanr este vector
   wb.load("./Archivos/Docentes.xlsx");
   for(int h=0;h< dias.size();h++){
     auto ws = wb.sheet_by_title(dias[h]);
@@ -104,35 +120,55 @@ int main(int argc, char const *argv[]) {
         aux++;
         if(aux>10){// se salta los bloques de descripcion
           VectorAux.push_back(cell.to_string()); //a un vector axiliar le estamos dando todos los datos
-
         }
       }
     }
-    for(int i=0; i<=VectorAux.size(); i=i+10)
-
+    for(int i=0; i<=VectorAux.size(); i=i+10)//se aumentan 10 celdas ya que
     {
-
+      if(h==0){//Lunes
       id_p =VectorAux[i];
-      nombre_p =VectorAux[i+1] +" "+ VectorAux[i+2];
-      b.push_back(VectorAux[i+3]);
-      b.push_back(VectorAux[i+4]);
-      b.push_back(VectorAux[i+5]);
-      b.push_back(VectorAux[i+6]);
-      b.push_back(VectorAux[i+7]);
-      b.push_back(VectorAux[i+8]);
-      b.push_back(VectorAux[i+9]);
+      nombre_p =VectorAux[i+1] +" "+ VectorAux[i+2];// dejamos el nombre y apellido juntos
+      b = {VectorAux[i+3],VectorAux[i+4],VectorAux[i+5],VectorAux[i+6],VectorAux[i+7],VectorAux[i+8],VectorAux[i+9]};
       Profesor aux(id_p,nombre_p,b);// le damos los parametros a un objeto auxiliar
       v.push_back(Profesor(aux));// agregamos el objeto a un vector de objetos
       b.clear(); //limpiamos el contenido del vector que indica la disponibilidad del horario
-      contador++;
-
-
+                          }
+      if (h==1){//martes
+        b = {VectorAux[i+3],VectorAux[i+4],VectorAux[i+5],VectorAux[i+6],VectorAux[i+7],VectorAux[i+8],VectorAux[i+9]};
+        v[i/10].set_martes(b);// el i/10 es para sacar el identificador del vector de profesores
+        b.clear();
       }
-    }
-    for(int j=0; j< v.size();j++){
-      v[j].mostrar();
-    }
-
-
-  return 0;
+      if(h==2){//miercoles
+        b = {VectorAux[i+3],VectorAux[i+4],VectorAux[i+5],VectorAux[i+6],VectorAux[i+7],VectorAux[i+8],VectorAux[i+9]};
+        v[i/10].set_miercoles(b);// el i/10 es para sacar el identificador del vector de profesores
+        b.clear();
+      }
+      if(h==3){//jueves
+        b = {VectorAux[i+3],VectorAux[i+4],VectorAux[i+5],VectorAux[i+6],VectorAux[i+7],VectorAux[i+8],VectorAux[i+9]};
+        v[i/10].set_jueves(b);// el i/10 es para sacar el identificador del vector de profesores
+        b.clear();
+      }
+      if(h==4){//viernes
+        b = {VectorAux[i+3],VectorAux[i+4],VectorAux[i+5],VectorAux[i+6],VectorAux[i+7],VectorAux[i+8],VectorAux[i+9]};
+        v[i/10].set_viernes(b);// el i/10 es para sacar el identificador del vector de profesores
+        b.clear();
+      }
+      if(h==5){//sabado
+        b = {VectorAux[i+3],VectorAux[i+4],VectorAux[i+5],VectorAux[i+6]};
+      /*  b.push_back(VectorAux[i+3]);// recopilando datos
+        b.push_back(VectorAux[i+4]);
+        b.push_back(VectorAux[i+5]);
+        b.push_back(VectorAux[i+6]);
+        b.push_back(VectorAux[i+7]);
+        b.push_back(VectorAux[i+8]);
+        b.push_back(VectorAux[i+9]);*/
+        v[i/10].set_sabado(b);// el i/10 es para sacar el identificador del vector de profesores
+        b.clear();
+      }
+      }
+      VectorAux.clear();// se limpia el vector axuliar para la siguiente iteracion
+      aux=0;//se limpia el contador axiliar para evitar tomar las cabeceras de los titulos
+  }
+v[0].mostrar();
+return 0;
 }
