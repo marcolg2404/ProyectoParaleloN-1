c9706104669a4c8abbf27f1ddefd7a9cffceb6d5
Sebaolivares11 <Sebastian.olivaresp11@gmail.com>
2019-06-07 01:46:38 -0400

    clase profesores, funcionando hasta el dia sabado

--- app/clases.cpp
+++ app/clases.cpp
@@ -79,37 +79,16 @@ void Profesor::set_viernes(vector<string> _vi){
 void Profesor::set_sabado(vector<string> _sa){
   sabado=_sa;
 }
-class Curso{ // declaracion clase curso;
-private:
-  string id_curso;
-  string nombre_curso;
-  int horas;
-  string id_doc;
-public:
-  Curso(string,string, int, string);//constructor
-  void mostrar_curso();
-};
+//Funciones
 
-Curso::Curso(string _id,string _nombre_c, int _horas, string _id_doc){
-  id_curso =_id;
-  nombre_curso = _nombre_c;
-  horas = _horas;
-  id_doc =_id_doc;
-}
-
-
-
-
-int main(int argc, char const *argv[]) {
+vector<Profesor> asignacion_profesores(){
   int aux=0;
-  vector<string> VectorAux;//vevtor para la clase profesor
-  vector<string> VectorAux_cursos;
   vector<Profesor> v;
+  vector<string> VectorAux;//vevtor para la clase profesor
   vector<string> b;
   string id_p,nombre_p;
-  int contador=0;
   workbook wb;
-  vector<string> dias = {"Lunes","Martes","Miércoles","Jueves","Viernes","Sábado"};// aca la idea esque cuando abra el archivo lleanr este vector
+  vector<string> dias = {"Lunes","Martes","Miércoles","Jueves","Viernes"};// aca la idea esque cuando abra el archivo lleanr este vector
   wb.load("./Archivos/Docentes.xlsx");
   for(int h=0;h< dias.size();h++){
     auto ws = wb.sheet_by_title(dias[h]);
@@ -153,22 +132,47 @@ int main(int argc, char const *argv[]) {
         v[i/10].set_viernes(b);// el i/10 es para sacar el identificador del vector de profesores
         b.clear();
       }
-      if(h==5){//sabado
-        b = {VectorAux[i+3],VectorAux[i+4],VectorAux[i+5],VectorAux[i+6]};
-      /*  b.push_back(VectorAux[i+3]);// recopilando datos
-        b.push_back(VectorAux[i+4]);
-        b.push_back(VectorAux[i+5]);
-        b.push_back(VectorAux[i+6]);
-        b.push_back(VectorAux[i+7]);
-        b.push_back(VectorAux[i+8]);
-        b.push_back(VectorAux[i+9]);*/
-        v[i/10].set_sabado(b);// el i/10 es para sacar el identificador del vector de profesores
-        b.clear();
-      }
-      }
+    }
       VectorAux.clear();// se limpia el vector axuliar para la siguiente iteracion
       aux=0;//se limpia el contador axiliar para evitar tomar las cabeceras de los titulos
   }
-v[0].mostrar();
+
+  return v;
+}
+void asignacion_profesores_sabado(vector<Profesor> &v){ //asignacion exclusiva para el dia sabado, le pasamos &v ya que nececitamos modificar el  vector
+  int aux=0;
+  vector<string> VectorAux;//vevtor para la clase profesor
+  vector<string> b;
+  string id_p,nombre_p;
+  workbook wb;
+  wb.load("./Archivos/Docentes.xlsx");
+  auto ws = wb.sheet_by_title("Sábado");
+
+  for(auto row : ws.rows(false))
+    {
+      for (auto cell:row)
+      {
+        aux++;
+        if(aux>7){// se salta los bloques de descripcion
+          VectorAux.push_back(cell.to_string()); //a un vector axiliar le estamos dando todos los datos
+        }
+      }
+    }
+    for(int i=0;i<VectorAux.size();i=i+7){
+      b = {VectorAux[i+3],VectorAux[i+4],VectorAux[i+5],VectorAux[i+6]};
+      v[i/7].set_sabado(b);// el i/10 es para sacar el identificador del vector de profesores
+      b.clear();
+    }
+}
+
+
+int main(int argc, char const *argv[]) {
+  vector<Profesor> listado;
+  listado=asignacion_profesores();
+  asignacion_profesores_sabado(listado);
+  listado[0].mostrar();
+  listado[22].mostrar();
+  listado[44].mostrar();
+
 return 0;
 }
