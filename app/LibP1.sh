f6b92468ccd444c4d6a492657096352b759b70e9
sebaolivares11 <sebastian.olivaresp11@gmail.com>
2019-06-14 20:37:01 -0400

    script de librerias

---
+++ app/LibP1.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#script para la instalacion de xlnt y xlsxwriter 
+
+#directorio general
+cd ~
+
+#xlnt
+
+#actualizamos repositorios
+sudo apt-get update
+sudo apt-get upgrade 
+sudo apt-get install cmake
+sudo apt-get install zlibc
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+sudo apt update
+sudo apt-get upgrade
+sudo apt-get install g++
+sudo apt-get install wget
+wget https://github.com/tfussell/xlnt/archive/master.zip
+cd ~
+unzip master.zip
+cd xlnt-master
+cmake .
+make -j 2
+sudo make install
+sudo ldconfig
+
+#segunda libreria xlsxwriter
+cd 
+sudo apt-get install -y zlib1g-dev
+git clone https://github.com/jmcnamara/libxlsxwriter.git 
+cd libxlsxwriter
+make 
+sudo make install
+
+#y eso es todo :D
